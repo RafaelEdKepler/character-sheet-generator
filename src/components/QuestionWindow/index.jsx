58e81af860145a38a21885d4fb135b79191cb11b
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2021-05-08 01:26:19 -0300

    First commit

---
+++ src/components/QuestionWindow/index.jsx
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+
+import { Container, ListContainer, RowContainer, Title, RowText, Img, 
+    Check, HelperContainer, TextHelper, HelperIconContainer } from "./style";
+
+const QuestionWindow = () => {
+    const [textHelper, setTextHelper] = useState('Isso aqui é um descritivo para verificar se o texto de ajuda está aparecendo corretamenteenteentennteentennteentennteentennteentennteentennteentennteentennteentennteentennteentennteenten');
+
+    const [desc, setDesc] = useState({
+        'Humano': 'Humanos são a raça mais populosa de Arton, não possuem nenhuma característica diferente dos humanos que você conhece.',
+        'Elfo': 'Elfos são criaturas altas e esguias, possuem algumas habilidade a mais, podendo enxergar melhor durante a noite, porém não costumam ser muito resistentes.',
+        'Anão': 'Anões são criaturas baixas e parrudas, possuem uma visão melhorada para se adaptar a vida nas minas, não costumam ser muito ágeis ou sorrateiros'
+    })
+    const [raca, setRaca] = useState([{
+            'value': 'Humano',
+        },{
+            'value': 'Elfo',
+        },{
+            'value': 'Anão'
+        }
+    ]);
+
+    const handleClick = (id) => {
+        setTextHelper(desc[id]);
+    }
+
+    return(
+        <Container>
+            <Title>Escolha sua raça!</Title>
+            <ListContainer>
+                    {raca.map((index) => 
+                        <RowContainer>
+                            <RowText>{index.value}</RowText>                    
+                            <Img
+                                onClick={() => handleClick(index.value)}
+                                id={index.value}
+                                src="./help_icon.svg"
+                            />
+                            <Check id={index.value} type="checkbox"/>
+                        </RowContainer>
+                    )}
+            </ListContainer>
+            <HelperContainer>
+                <RowContainer>
+                    <HelperIconContainer>
+                        <Img
+                            src="./help_icon.svg"
+                        />
+                    </HelperIconContainer>
+                </RowContainer>
+                <TextHelper readonly value={textHelper}/>
+            </HelperContainer>
+        </Container>
+    );
+}
+
+export default QuestionWindow;
---
+++ src/components/QuestionWindow/style.jsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+import px2vw from "../../utils/px2vw";
+
+export const Container = styled.div`
+    width: ${px2vw(513)};
+    height: ${px2vw(404)};
+    background: var(--grey);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: ${px2vw(10)};
+    border-style: solid;
+`;
+
+export const ListContainer = styled.div`
+    width: ${px2vw(445)};
+    height: ${px2vw(149)};
+    margin: ${px2vw(10)};    
+    background: #FFF;    
+    display: flex;
+    flex-direction: column;
+    border-style: solid;
+    padding: ${px2vw(5)};
+    border-radius: 5px;
+    overflow-x: hidden;
+`;
+
+export const RowContainer = styled.div`
+    width: ${px2vw(400)};
+    height: ${px2vw(25)};
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+`;
+
+export const Title = styled.span`
+    font-size: 1.5rem;
+    margin: 10px 0 20px 0;
+`;
+
+export const RowText = styled.span`
+    width: ${px2vw(342)};
+    overflow: hidden;
+`
+export const Img = styled.img`
+    width: ${px2vw(20)};
+    height: ${px2vw(20)};    
+    margin: ${px2vw(3)} 0 0 0;
+`;
+
+export const Check = styled.input`
+    width: ${px2vw(20)};
+    height: ${px2vw(20)};
+    border-radius: ${px2vw(5)};
+`
+
+export const HelperContainer = styled(ListContainer)`
+    height: ${px2vw(109)};
+`;
+
+export const HelperIconContainer = styled.div`
+    display: flex;    
+    justify-content: left;    
+    width: 100%;
+`;
+
+export const TextHelper = styled.textarea`
+    width: ${px2vw(430)};
+    height: ${px2vw(90)};
+    resize: none;
+    border-style: none;
+
+    &:focus {
+        outline: 0;
+    }
+`
