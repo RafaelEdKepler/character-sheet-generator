570f337b9bb986e1c76823eb648b70195c55de04
Rafael Eduardo Kepler <rafael.kepler@hotmail.com>
2021-05-11 22:24:48 -0300

    Adicionando models

--- pages/api/database/migrations/1620697443456-CreateClasses.ts
+++ pages/api/database/migrations/1620697443456-CreateClasses.ts
@@ -1,8 +1,14 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateClasses1620697443456 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "classes",
+
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
